46c8d6b14973aecaa39d976e79d75a651a0738a6
Mohith Pranav <pranavmohith@gmail.com>
2026-01-11 18:31:05 +0530

    env modified

--- src/services/labelService.js
+++ src/services/labelService.js
@@ -1,6 +1,6 @@
 import { getCookie } from "../utils/cookieUtils";
 
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
 
 const getAuthHeaders = () => {
   const token = getCookie("authToken");
